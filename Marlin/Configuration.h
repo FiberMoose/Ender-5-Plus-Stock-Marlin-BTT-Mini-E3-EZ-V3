380aec62942e59443f6739685183f82c5448f5a9
FiberMoose <Fibermoose@protonmail.com>
2026-06-24 18:42:25 -0500

    bump bed size to 358x370 for full e5+ travel + add skr mini e3 v3 build workflow

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1712,8 +1712,8 @@
 // @section geometry
 
 // The size of the printable area
-#define X_BED_SIZE 350
-#define Y_BED_SIZE 350
+#define X_BED_SIZE 358
+#define Y_BED_SIZE 370
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
 #define X_MIN_POS  8
